b600cc185d31838f2e9ae17fa1d38b23d0a5b73c
Chris Burgess <chris@giantrobot.co.nz>
2017-05-24 20:38:45 +1200

    Adjust logging call
    Use `Civi::log()->info()` not `$this->ctx->log->info()`

--- CRM/Paystation/Upgrader.php
+++ CRM/Paystation/Upgrader.php
@@ -8,7 +8,7 @@ class CRM_Paystation_Upgrader extends CRM_Paystation_Upgrader_Base {
    * Convert from 3.x-style specialized extension to a 4.x-style general ext.
    */
   public function upgrade_3000() {
-    $this->ctx->log->info('Applying update 4200');
+    Civi::log()->info('nz.co.fuzion.paystation: Applying update 3000');
     CRM_Core_DAO::executeQuery('UPDATE civicrm_extension SET type = "module" WHERE full_name = "nz.co.fuzion.paystation"');
     return TRUE;
   }
