043f28e082b320b280a093756d39acfddf33f1fc
Chris Burgess <chris@giantrobot.co.nz>
2017-05-24 20:55:39 +1200

    Trigger managed entity things.
    Refs #9.

--- CRM/Paystation/Upgrader.php
+++ CRM/Paystation/Upgrader.php
@@ -12,4 +12,16 @@ class CRM_Paystation_Upgrader extends CRM_Paystation_Upgrader_Base {
     CRM_Core_DAO::executeQuery('UPDATE civicrm_extension SET type = "module" WHERE full_name = "nz.co.fuzion.paystation"');
     return TRUE;
   }
+
+  /**
+   * Use System.flush to clear caches to trigger hook_civicrm_managed() and
+   * update managed values.
+   *
+   * This might not be required on all sites, but it's harmless enough.
+   */
+  public function upgrade_3001() {
+    Civi::log()->info('nz.co.fuzion.paystation: Executing System.flush');
+    civicrm_api3('System', 'flush', []);
+    return TRUE;
+  }
 }
